9230d58a7d83e260b08711d38a14dc883f8aef2a
aCupOfHotJava <bkrieg1999@gmail.com>
2021-09-19 19:37:52 -0700

    Began implementation of snow - did some functionality brainstorming

--- snow.py
+++ snow.py
@@ -1,2 +1,16 @@
 # Implement snow feature for weather suggestion appplication
 # @ Brandon Krieg
+
+# driver will call snow function with ground conditions,
+# precipitation chance, and wind speed. snow will return a list
+# of wares that are recommended given the conditions
+
+def snow(ground, precip, wind):
+    wares = {}
+    if(ground == "ice"):
+        wares.append("Crampons", "Boots")
+    elif(ground == "slush"):
+        wares.append("Rubber boots")
+    else:
+        wares.append("Regular shoes")
+    # TODO
