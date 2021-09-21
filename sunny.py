9282f975b3df00bfc5159813ebe1821789c69610
sophiaj5 <59673737+sophiaj5@users.noreply.github.com>
2021-09-20 23:37:05 -0800

    added function to sunny.py and unit test

--- sunny.py
+++ sunny.py
@@ -1,1 +1,12 @@
 # going to implement a feature for sunny weather - Sophia
+
+#function to print statements on if it will be sunny or not based on the weather (in degrees Celsius)
+def chancesOfSun(weather):
+    if weather < 15:
+        print("It will most likely not be sunny today")
+    elif weather > 16 and weather < 20:
+        print("It may be sunny today")
+    else:
+        print("There is a high chance it will be sunny today!") 
+
+#chancesOfSun(14)
