43f5645df26a5fff4339827ebc9c998de0e45248
aCupOfHotJava <bkrieg1999@gmail.com>
2021-09-21 11:20:39 -0700

    finished snow feature

--- snow.py
+++ snow.py
@@ -5,12 +5,23 @@
 # precipitation chance, and wind speed. snow will return a list
 # of wares that are recommended given the conditions
 
-def snow(ground, precip, wind):
-    wares = {}
+# no exception handling or input checks, just a quick program
+# for the group assignment
+
+def snow(ground, precip, temperature):
+    wares = []
     if(ground == "ice"):
-        wares.append("Crampons", "Boots")
+        wares.append("Crampons")
+        wares.append("Boots")
     elif(ground == "slush"):
         wares.append("Rubber boots")
     else:
         wares.append("Regular shoes")
-    # TODO
+    if(precip > 0.5):
+        wares.append("Hood")
+    if(temperature < 0):
+        wares.append("Heavy jacket")
+    else:
+        wares.append("Light jacket")
+    print("You should wear: ")
+    print(*wares, sep = ", ")
